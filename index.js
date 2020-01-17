2e22ad8bcef7446e5669dd2555dca490d5455719
Han <hanky_chung@hotmail.com>
2020-01-17 18:35:58 +0800

    update tip

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class Composer {
         return Promise.resolve()
       }
       let middleware = this.middlewares[idx]
-      // 这里的next函数作为参数，必须返回dispatch(idx + 1)，以供await next()执行，否则next()返回undefined，next里的异步操作也不会被await
+      // 这里的next函数作为参数，必须返回dispatch(idx + 1)，以供await next()，否则next()返回的是undefined而不是异步函数，await将失去其作用
       return Promise.resolve(middleware(this.ctx, () => dispatch(idx + 1)))
     }
     dispatch(0)
